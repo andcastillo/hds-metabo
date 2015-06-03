0872e9c17f1d4b9132499761bc9e8f6903d2a544
andcastillo <andcastillo@gmail.com>
2015-06-03 16:58:58 -0500

    Import update
    amc

--- imports/import_TBMN.js
+++ imports/import_TBMN.js
@@ -26,12 +26,15 @@ fs.createReadStream('../data/data.csv').pipe(parser);
 function onSuccess(data) {
 
     Entry.findOne('project', {
-        _id: "5568edafba5c2b7e3f392147"
+        _id: "556f6ae067b0a7c40d9ffa09"
     }).exec().then(function (TBMN) {
         if (TBMN) {
             //Create the subject to study
             var nPatients = 10;//data.length - 1; //field 0 of data is header
             var nSamples = 1;
+            var header = data[0];
+            var promises = [];
+            var nPromises = nPatients*nSamples;
             for (var i = 1; i <= nPatients; i++) { //1 indexed because field 0 of data is header
                 var row = data[i];
                 var patient = TBMN.createChild('patient', {
@@ -40,14 +43,22 @@ function onSuccess(data) {
                     weight: row[5],
                     height: row[6],
                     BMI: row[7]
-                }).save().then(function (patient, row) {
+                }).save().then(function (patient) {
                     console.log("patient created");
                     //create clinic children for diseases
-                    createDiseases(patient,row);
+                    createDiseases(patient, header, row).then(function(){
+                        nPromises--;
+                        console.log(nPromises);
+                        if(nPromises == 0){
+                            console.log("Ok");
+                            process.exit(0);
+                        }
+                    });
                     //create nmr objects
-                    createSpectra(patient,row);
+                    //createSpectra(patient,row);
                 });
             }
+
             //console.log(TBMN);
             //process.exit(0);
             //return exp.getChildren({kind: 'entity'});
@@ -60,42 +71,64 @@ function onError(){
     console.log("Error, as usual, unexpected");
 }
 
-
-function createDiseases(patient,row) {
+function createDiseases(patient, header, row) {
     console.log("generating clinic history");
+    var diagnosis = [];
+    var treatments = [];
+
+    console.log(row);
+    //Columns 8 to 12: Diabetes,	HT,	Hyperlipidemia,	IHD,	Smoking,
     for (var j = 8; j <= 12; j++) {
-        if (typeOf(row[j]) == "string") {
-            if (parseInt(row[j].substring(0, 1)) == 1) {
-                patient.createChild('clinic', {
-                    name: row[0],
-                    info: row[j].substring(1, row[j].length)
-                }).save();
+
+        if (row[j] && (typeof row[j] == "string") && row[j].length > 1) {
+            try{
+
+                if (parseInt(row[j].substring(0, 1)) == 1) {
+                    diagnosis.push({name:header[j],comments:row[j].substring(1, row[j].length)});
+                }
+            }
+            catch(e){
+                console.log("We expected a numeric value as first character but it was not found");
             }
         }
         else {
             if (row[j] == 1) {
-                patient.createChild('clinic', {
-                    name: row[0],
-                }).save();
+                diagnosis.push({name:header[j]});
             }
         }
     }
-
+    //Columns 13 to 15: Other diseases
     for (var j = 13; j <= 15; j++) {
-        patient.createChild('clinic', { name: row[j] }
-        ).save();
+        if (row[j] && (typeof row[j] == "string") && row[j].length > 1) {
+            diagnosis.push({name:row[j].trim()});
+        }
     }
 
-    for(var j = 15; j <= 22; j++) {
-        patient.createChild('clinic', {
-            name: row[0],
-            value: row[j]
-        }).save();
+    //Save the treatments. We don't know the relation among the reported diseases and the treatments
+    if(row[16]&&(typeof row[16] ==="string")&&row[16].length>1){
+        treatments = row[16].split(",");
     }
 
-    console.log("clinic history saved");
+    //Define the period
+    var period = {from: new Date("01/01/2013"), to: new Date("01/06/2013")};
+    var controls = [];
+    for(var j = 17; j <= 24; j++) {
+        var control = {
+            name:header[j].substring(0, header[j].indexOf("(")),
+            period:period
+        };
+        if(row[j] && typeof row[j] ==="numeric" )
+            control.numericValue = row[j];
+        else{
+            if(row[j] && typeof row[j] ==="string" )
+                control.stringValue = row[j];
+        }
+        constrols.push(control);
+    }
+    console.log("clinic history created");
+    return patient.createChild('clinic',{diagnosis:diagnosis,controls:controls,treatments:treatments}).save();
 }
-
+/*
 function createSpectra(patient,row) {
     console.log("generating sample");
     var sample = patient.createChild('sample', {
@@ -129,3 +162,4 @@ function createSpectra(patient,row) {
     }).save();
     console.log("spectra saved");
 }
+*/
